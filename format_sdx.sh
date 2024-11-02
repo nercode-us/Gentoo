9e3827a22b2395c1806986bffc4abb33c43ca33e
nercode <nerka020@gmail.com>
2024-11-02 14:38:23 +0300

    Update format_sdx.sh

--- format_sdx.sh
+++ format_sdx.sh
@@ -1,10 +1,10 @@
-echo -n "Iveskite disko pavadinima /dev/sdX: "
+echo -n "Disk  /dev/sdX: "
 read disk
 size=`fdisk -l | grep 'Disk' | grep $disk | awk '{print $3,$4}' | tr -d ',' | tr -s '\.' ' ' | awk '{print $1,$3}'`
 echo "Disko dydis:" $size
 
 
-echo -n "Iveskite /boot dydi Gib skaiciais (1...9): "
+echo -n "/boot size in Gib numbers (1...9): "
 read bts
 btz="+"$bts"G"
 (echo n ; echo p ; echo 1 ; echo ; echo $btz ; echo t ; echo 82 ; echo a ; echo p) | fdisk $disk >> out.txt
@@ -13,7 +13,7 @@ cat out.txt | grep $boot
 rm -f out.txt
 
 
-echo -n "Iveskite /swap dydi Gib skaiciais (1...16): "
+echo -n "/swap size Gib numbers (1...16): "
 read sws
 swz="+"$sws"G"
 
