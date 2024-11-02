fc968e62ab803799e8a453a10a619191d0ab23b2
nercode <nerka020@gmail.com>
2024-11-02 14:43:35 +0300

    Update format_sdx.sh

--- format_sdx.sh
+++ format_sdx.sh
@@ -7,7 +7,7 @@ echo "Disko dydis:" $size
 echo -n "/boot size in Gib numbers (1...9): "
 read bts
 btz="+"$bts"G"
-(echo n ; echo p ; echo 1 ; echo ; echo $btz ; echo t ; echo 82 ; echo a ; echo p) | fdisk $disk >> out.txt
+(echo n ; echo p ; echo 1 ; echo ; echo $btz ; echo a ; echo p) | fdisk $disk >> out.txt
 boot=$disk"1"
 cat out.txt | grep $boot
 rm -f out.txt
@@ -16,6 +16,11 @@ rm -f out.txt
 echo -n "/swap size Gib numbers (1...16): "
 read sws
 swz="+"$sws"G"
+(echo n ; echo p ; echo 1 ; echo ; echo $sws ; echo t ; echo 82 ; echo p) | fdisk $disk >> out.txt
+swap=$disk"2"
+cat out.txt | grep $swap
+rm -f out.txt
+
 
 
 xx=`echo $size | awk '{print $1}'`
