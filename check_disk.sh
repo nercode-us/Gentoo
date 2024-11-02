1d61ee7832c45a9c04682eded3564e40bee030f0
nercode <nerka020@gmail.com>
2024-11-02 13:37:06 +0300

    Update check_disk.sh

--- check_disk.sh
+++ check_disk.sh
@@ -1,4 +1,5 @@
 echo -n "Iveskite disko pavadinima (/dev/sdX):"
+fdisk -l | grep '/dev' | grep -v 'loop'
 read dsk
 gp=`(echo p) | fdisk $dsk | grep Disklabel | awk '{print $3}' | sed -n '1p'`
 if [ "$gp" == "gpt" ];
