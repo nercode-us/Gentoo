1626edfa9e695c35def2eece204b2f27b0dc9bed
nercode <nerka020@gmail.com>
2024-11-04 14:35:29 +0200

    Update cat-uses.sh

--- cat-uses.sh
+++ cat-uses.sh
@@ -2,4 +2,4 @@
 echo "cat /mnt/gentoo/etc/portage/make.conf"
 cat /mnt/gentoo/etc/portage/make.conf | grep -v -e '\#' -e '\$' | grep '.'
 echo "/etc/portage/make.conf"
-cat /etc/portage/make.conf
+cat /etc/portage/make.conf | grep -v -e '\#' -e '\$' | grep '.'
