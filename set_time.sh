87a8132d79fddbddd0f209e5b8e1702a895c7920
nercode <nerka020@gmail.com>
2024-11-04 12:11:18 +0200

    Update set_time.sh

--- set_time.sh
+++ set_time.sh
@@ -1,6 +1,9 @@
 # https://timeapi.io/swagger/index.html
 # https://www.cyberciti.biz/faq/howto-set-date-time-from-linux-command-prompt/
+home=`pwd`
 cd /mnt/gentoo
 
 date +%T -s "`curl --silent -X 'GET' 'https://timeapi.io/api/time/current/zone?timeZone=Europe%2FVilnius' -H 'accept: application/json' | \
 tr ',' '\n' | grep 'dateTime'| sed 's/Time/time/g' | tr -s 'T\.' '\n' | grep -v '"'`"
+
+cd $home
