7991c8141779c9d01bef6d1c38038f735296fee8
nercode <nerka020@gmail.com>
2024-11-05 08:32:56 +0200

    Add files via upload

---
+++ mount_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
+
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
+#UEFI
+#mount /dev/sda1 /efi
+
+#MBR
+mount /dev/sda1 /boot
