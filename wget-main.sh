f275a360a0ec35cc70c176c785aa079e2f7ce2b2
nercode <nerka020@gmail.com>
2024-11-05 18:46:58 +0200

    Update wget-main.sh

--- wget-main.sh
+++ wget-main.sh
@@ -1,5 +1,4 @@
-wget https://github.com/nercode-us/Gentoo/raw/refs/heads/main/Gentoo-main.zip
 cd ..
 rm -f Gentoo-main.zip
+wget https://github.com/nercode-us/Gentoo/raw/refs/heads/main/Gentoo-main.zip
 cd Gentoo-main/
-mv Gentoo-main.zip ../
