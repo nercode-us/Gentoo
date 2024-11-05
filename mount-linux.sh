749208ab03d60f5e6596adc530fb2c7c9fe8077b
nercode <nerka020@gmail.com>
2024-11-05 10:28:56 +0200

    Update mount-linux.sh

--- mount-linux.sh
+++ mount-linux.sh
@@ -18,7 +18,7 @@ export PS1="(chroot) ${PS1}"
 mount /dev/sda1 /boot
 # Moving lost files again to home directory
 cd ~
-wget "https://github.com/nercode-us/Gentoo/raw/refs/heads/main/Gentoo-main.zip" > /dev/null 2>&1
+wget -q "https://github.com/nercode-us/Gentoo/raw/refs/heads/main/Gentoo-main.zip"
 unzip Gentoo-main.zip > /dev/null 2>&1
 cd Gentoo-main/
 bash enter-start.sh
