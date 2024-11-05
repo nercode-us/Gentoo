7db18111e5d0be6e1ce48f81706aa178ccc87811
nercode <nerka020@gmail.com>
2024-11-05 10:46:10 +0200

    Update set-configs.sh

--- set-configs.sh
+++ set-configs.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 conf="/mnt/gentoo/etc/portage/make.conf"
 
-xc=`cat $conf | grep -n 'COMMON_FLAGS="' | tr ':' ' ' | awk '{print $1}'`
-xl=`cat $conf | sed -n $xc'p' | tr -d '"' | sed 's/COMMON_FLAGS=//g'`
-
 x=`nproc`
 y=$(($x+1))
 mkf="MAKEOPTS=\"-j$x -l$y\""
 
-echo $mkf >> /mnt/gentoo/etc/portage/make.conf
+echo $mkf >> $conf
 
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
