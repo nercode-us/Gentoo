f9c06e13f5a17994e172b79d1c7dead35f55a047
nercode <nerka020@gmail.com>
2024-11-05 10:33:13 +0200

    Update mount-linux.sh

--- mount-linux.sh
+++ mount-linux.sh
@@ -17,6 +17,24 @@ export PS1="(chroot) ${PS1}"
 #MBR
 mount /dev/sda1 /boot
 # Moving lost files again to home directory
+cnf="/etc/portage/make.conf"
+
+xc=`cat $cnf | grep -n 'COMMON_FLAGS="' | tr ':' ' ' | awk '{print $1}'`
+xl=`cat $cnf | sed -n $xc'p' | tr -d '"' | sed 's/COMMON_FLAGS=//g'`
+
+cpuname=`gcc -march=native -Q --help=target | grep 'march=' | grep -v 'option' | awk '{print $2}'`
+cpo="-march="$cpuname
+cpf="COMMON_FLAGS=\"$cpo $xl\""
+
+echo $cpf
+
+sed $xc'c\'"$cpf" /etc/portage/make.conf >> /etc/portage/make.conf.new
+
+
+mv /etc/portage/make.conf.new /etc/portage/make.conf
+
+
+
 cd ~
 wget -q "https://github.com/nercode-us/Gentoo/raw/refs/heads/main/Gentoo-main.zip"
 unzip Gentoo-main.zip > /dev/null 2>&1
