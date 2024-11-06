c0119d1fd8cb5a439d4bfcf393e5ff9ca153c7e8
nercode <nerka020@gmail.com>
2024-11-06 07:00:12 +0200

    Update profile-set.sh

--- profile-set.sh
+++ profile-set.sh
@@ -1,2 +1,5 @@
+echo -n "Please wait ... "
 es=`eselect profile list | grep desktop | grep stable | sed -n '1p' | awk '{print $1}' | tr -d '[]'`
 eselect profile set $es
+getuto > /dev/null/ 2>&1
+echo "Done."
