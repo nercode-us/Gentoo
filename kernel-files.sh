4289f27ec5cb7f328ca71665cb2bec858296f5e2
nercode <nerka020@gmail.com>
2024-11-06 09:24:08 +0200

    Update kernel-files.sh

--- kernel-files.sh
+++ kernel-files.sh
@@ -1,8 +1,11 @@
 emerge sys-kernel/linux-firmware
 emerge sys-firmware/sof-firmware
+
+
 emerge sys-kernel/installkernel
 emerge sys-kernel/gentoo-kernel
-
 emerge sys-kernel/gentoo-sources
+
+
 emerge sys-apps/pciutils
 emerge app-crypt/sbsigntools
