640792e85ff2e772fadf020652ec283c51880a89
nercode <nerka020@gmail.com>
2024-11-06 16:05:44 +0200

    Update local-gentoo.sh

--- local-gentoo.sh
+++ local-gentoo.sh
@@ -5,4 +5,6 @@
 ln -sf ../usr/share/zoneinfo/Europe/Vilnius /etc/localtime
 
 locale-gen
-env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+env-update
+
+# && source /etc/profile && export PS1="(chroot) ${PS1}"
