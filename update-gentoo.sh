527783664638ef296966dd1ae23cdd49b4505f2a
nercode <nerka020@gmail.com>
2024-11-06 06:01:26 +0200

    Add files via upload

---
+++ update-gentoo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# https://wiki.gentoo.org/wiki/Mirrorselect
+# https://dev.gentoo.org/~zmedico/portage/doc/man/emerge.1.html
+# https://www.gentoo.org/downloads/mirrors/
+
+emerge-webrsync
+emerge --sync
+
+yes y | emerge --ask --update sys-apps/portage
+mkdir /etc/portage/repos.conf
+cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
+
+yes y | emerge --ask --verbose --oneshot app-portage/mirrorselect
+mirrorselect -s3 -b10 -D
