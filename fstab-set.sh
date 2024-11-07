bbd147b557072cded590ba6e89c1ce65eaaa9064
nercode <nerka020@gmail.com>
2024-11-07 15:09:38 +0200

    Add files via upload

---
+++ fstab-set.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+fdir="/etc/fstab"
+echo -e "/dev/sda1   /boot        xfs    defaults    0 2" >> $fdir
+echo -e "/dev/sda2   none         swap    sw                   0 0" >> $fdir
+echo -e "/dev/sda3   /            xfs    defaults,noatime              0 1" >> $fdir
+echo "" >> $fdir
+echo -e "/dev/cdrom  /mnt/cdrom   auto    noauto,user          0 0" $fdir
