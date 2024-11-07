1c08f6bf48fb76cafa10768b8b0afda7e0fc5761
nercode <nerka020@gmail.com>
2024-11-07 16:21:18 +0200

    Update net-configs.sh

--- net-configs.sh
+++ net-configs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 name="tux"
 echo $name > /etc/hostname
-
-emerge net-misc/dhcpcd
-
-# rc-update add dhcpcd default
-# rc-service dhcpcd start
-
+#-----------------------------------
 emerge --noreplace net-misc/netifrc
-
+#-----------------------------------
+cd /etc/init.d
+ln -s net.lo net.$nnc
+rc-update add net.$nnc default
 #-----------------------------------
 # Network settings, for dhcp
 #
@@ -16,11 +14,13 @@ conf="/etc/conf.d/net"
 nnc=`ip a | grep 'state UP' | tr -d ':' | awk '{print $2}'`
 
 dp="config_"$nnc"=\"dhcp\""
-
 echo $dp >> $conf
 
-#----------------------------------- 
+#-----------------------------------
 
-cd /etc/init.d
-ln -s net.lo net.$nnc
-rc-update add net.$nnc default
+emerge net-misc/dhcpcd
+rc-update add dhcpcd default
+rc-service dhcpcd start
+
+
+
