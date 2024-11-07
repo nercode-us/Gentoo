a7a0f26ce861b4f72aff8b58544b964c9b558593
nercode <nerka020@gmail.com>
2024-11-07 07:33:15 +0200

    Update set-configs.sh

--- set-configs.sh
+++ set-configs.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 conf="/mnt/gentoo/etc/portage/make.conf"
-
+#=================================
 x=`nproc`
 y=$(($x+1))
 mkf="MAKEOPTS=\"-j$x -l$y\""
 
 echo $mkf >> $conf
+#=================================
+vm="vmware"
+vb="virtualbox"
+vgpu="VIDEO_CARDS=\"$vm $vb\""
+
+echo $vgpu >> $conf
+
+#=================================
 
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
