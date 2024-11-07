d0bc7da371b577b185986aef7354ae067a754d7a
nercode <nerka020@gmail.com>
2024-11-07 22:09:17 +0200

    Create add-users.sh

---
+++ add-users.sh
@@ -0,0 +1,10 @@
+echo "Vartotojo <root> pasword:"
+passwd root
+uname="friend"
+echo -n "Iveskite vartotojo <friend> password:"
+useradd -m -G users,wheel,audio -s /bin/bash $uname
+passwd friend
+
+gpasswd -a $uname vboxguest
+gpasswd -a $uname vboxsf
+gpasswd -a $uname vboxusers
