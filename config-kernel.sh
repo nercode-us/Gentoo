813fb261344d4e5787ad9c73ccbd14f730581f05
nercode <nerka020@gmail.com>
2024-11-07 10:42:11 +0200

    Update config-kernel.sh

--- config-kernel.sh
+++ config-kernel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+ek=`eselect kernel list | grep gentoo | grep linux | sed -n '1p' | awk '{print $1}' | tr -d '[]'`
+eselect kernel set $ek
+
 cd /usr/src/linux
 make localmodconfig
 make menuconfig
