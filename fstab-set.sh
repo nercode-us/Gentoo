0f1b9df6426d767108fb2ef48c3612b084c2e8d8
nercode <nerka020@gmail.com>
2024-11-07 15:48:34 +0200

    Update fstab-set.sh

--- fstab-set.sh
+++ fstab-set.sh
@@ -3,5 +3,5 @@ fdir="/etc/fstab"
 echo -e "/dev/sda1   /boot        xfs    defaults    0 2" >> $fdir
 echo -e "/dev/sda2   none         swap    sw                   0 0" >> $fdir
 echo -e "/dev/sda3   /            xfs    defaults,noatime              0 1" >> $fdir
-echo "" >> $fdir
-echo -e "/dev/cdrom  /mnt/cdrom   auto    noauto,user          0 0" $fdir
+echo -e "\n" >> $fdir
+echo -e "/dev/cdrom  /mnt/cdrom   auto    noauto,user          0 0" >> $fdir
