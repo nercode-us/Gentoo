a99054a65362ab3a684cffcd46c6b628c8e59bb8
nercode <nerka020@gmail.com>
2024-11-07 22:11:28 +0200

    Update add-users.sh

--- add-users.sh
+++ add-users.sh
@@ -2,9 +2,9 @@
 echo "Vartotojo <root> pasword:"
 passwd root
 uname="friend"
-echo -n "Iveskite vartotojo <friend> password:"
+echo -n "Iveskite vartotojo <$uname> password:"
 useradd -m -G users,wheel,audio -s /bin/bash $uname
-passwd friend
+passwd $uname
 
 gpasswd -a $uname vboxguest
 gpasswd -a $uname vboxsf
