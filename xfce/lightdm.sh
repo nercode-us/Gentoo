c3ed54e0192330a948c8a01b1b48a087f4558b0b
nercode <nerka020@gmail.com>
2024-11-08 15:53:49 +0200

    Add files via upload

---
+++ xfce/lightdm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+emerge x11-misc/lightdm
+emerge gui-libs/display-manager-init
+
+cf='/etc/conf.d/display-manager'
+cfnew='/etc/conf.d/display-manager.new'
+n=`cat $cf | grep -n 'DISPLAYMANAGER' | tr -s ':' ' ' | awk '{print $1}' | sed -n '1p'`
+
+ldm="DISPLAYMANAGER=\"lightdm\""
+
+sed $n'c\'"$ldm" $cf >> $cfnew
+mv $cfnew $cf
+
+rc-update add dbus default
+rc-update add display-manager default
+
+rc-service dbus start
+rc-service display-manager start
