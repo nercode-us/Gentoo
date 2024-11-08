77139dbc65bbf51c645da370c417ba962547261a
nercode <nerka020@gmail.com>
2024-11-08 10:36:35 +0200

    Update virtual-box.sh

--- virtual-box.sh
+++ virtual-box.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-emerge app-emulation/virtualbox
-emerge app-emulation/virtualbox-modules
 
-emerge app-emulation/virtualbox-additions
-emerge app-emulation/virtualbox-guest-additions
-
+emerge app-emulation/virtualbox-modules app-emulation/virtualbox-additions app-emulation/virtualbox-guest-additions
+#==================================================
 rc-update add virtualbox-guest-additions default
 rc-update add dbus default
-
+#==================================================
 emerge sys-apps/usermode-utilities
 emerge net-misc/bridge-utils
 
