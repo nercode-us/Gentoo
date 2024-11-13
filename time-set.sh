f0510ec9eeaee4fcac8098dbe245db6303924c4b
nercode <nerka020@gmail.com>
2024-11-13 14:08:27 +0200

    Add files via upload

---
+++ time-set.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# https://timeapi.io/swagger/index.html
+# https://www.cyberciti.biz/faq/howto-set-date-time-from-linux-command-prompt/
+hm=`pwd`
+cd /mnt/gentoo
+
+chronyd -q > /dev/null 2>&1
+
+kalen=`curl --silent -X 'GET' 'https://timeapi.io/api/time/current/zone?timeZone=Europe%2FVilnius' -H 'accept: application/json' | tr ',{}' '\n' | grep '"date"' | tr '"\/' ' ' | awk '{print $5,$3,$4}' | tr -d '^\n '`
+
+date +%m%d%Y -s "$kalen" > /dev/null 2>&1
+
+
+laik=`curl --silent -X 'GET' 'https://timeapi.io/api/time/current/zone?timeZone=Europe%2FVilnius' -H 'accept: application/json' | \
+tr ',{}' '\n' | grep 'dateTime'| sed 's/Time/time/g' | tr -s 'T\.' '\n' | grep -v '"'`
+
+date +%T -s "$laik" > /dev/null 2>&1
+
+hwclock --systohc
+cd $hm
