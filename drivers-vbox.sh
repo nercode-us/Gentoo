acb3216f5bb6eeeb7e232a9edc8d8fa69316e411
nercode <nerka020@gmail.com>
2024-11-13 09:05:44 +0200

    Add files via upload

---
+++ drivers-vbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Video and Input Drivers for VMWare and VirtualBox
+emerge -v x11-drivers/xf86-video-vmware
+emerge -v x11-drivers/xf86-video-vboxvideo
+emerge -v x11-drivers/xf86-input-vmmouse
+emerge -v x11-drivers/xf86-video-fbdev
