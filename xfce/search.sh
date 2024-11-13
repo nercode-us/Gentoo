bb8c2c5b5c39279afa5ea72b5bf6229d37bd87a3
nercode <nerka020@gmail.com>
2024-11-13 03:04:32 +0200

    Update search.sh

--- xfce/search.sh
+++ xfce/search.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 emerge -s $1 | grep -v -e 'License' -e 'Description' -e 'available' \
 | sed 's/Latest version installed: //g' | sed 's/Size of files: //g' | sed 's/Homepage:      //g' \
 | sed 's/\[ Not Installed \]/\{Not Installed\}/g' | sed 's/\[ Masked \]/\{Masked\}/g' | tr -s '\n' ' ' \
