46c1493596ed93cb1e478d5be093925aa3e81cda
nercode <nerka020@gmail.com>
2024-11-14 21:22:44 +0200

    Update drivers-vbox.sh

--- drivers-vbox.sh
+++ drivers-vbox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Video and Input Drivers for VMWare and VirtualBox
 #===============================================================
+emerge -v x11-drivers/xf86-video-fbdev --autounmask-write
 emerge -v x11-drivers/xf86-input-vmmouse --autounmask-write
 emerge -v x11-drivers/xf86-video-vmware --autounmask-write
 
@@ -14,4 +15,3 @@ echo u | dispatch-conf
 emerge -v x11-drivers/xf86-video-vboxvideo
 #===============================================================
 
-emerge -v x11-drivers/xf86-video-fbdev
