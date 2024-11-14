1b7262fce6a0e4970e8a58ebce12acdffcfe595c
nercode <nerka020@gmail.com>
2024-11-14 19:22:54 +0200

    Update kernel-files.sh

--- kernel-files.sh
+++ kernel-files.sh
@@ -6,8 +6,10 @@ emerge sys-kernel/installkernel
 emerge sys-kernel/gentoo-sources
 
 emerge --config sys-kernel/gentoo-kernel
-emerge --config sys-kernel/gentoo-kernel-bin
-
+#===================================================
+emerge sys-kernel/gentoo-kernel-bin --autounmask-write
+echo u | dispatch-conf
+#===================================================
 emerge sys-kernel/modprobed-db
 emerge sys-apps/pciutils
 emerge app-crypt/sbsigntools
